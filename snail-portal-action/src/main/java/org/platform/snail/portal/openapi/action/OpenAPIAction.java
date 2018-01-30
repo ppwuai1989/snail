58e1aacafab93aeb220b710ae9d19a9de6835f45
ppwuai1989 <35420277+ppwuai1989@users.noreply.github.com>
2018-01-30 20:56:10 +0800

    ,,,

---
+++ snail-portal-action/src/main/java/org/platform/snail/portal/openapi/action/OpenAPIAction.java
@@ -0,0 +1,96 @@
+package org.platform.snail.portal.openapi.action;
+
+import java.io.Serializable;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.platfom.snail.pay.model.PaySaPi;
+import org.platform.snail.beans.DataResponse;
+import org.platform.snail.portal.service.OpenAPIService;
+import org.platform.snail.utils.PayUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import net.sf.json.JSONObject;
+
+/**
+ * @author pzp 对外开放的公用业务接口
+ */
+@Controller
+@RequestMapping(value = "/openAPI")
+public class OpenAPIAction implements Serializable {
+
+	@Autowired
+	private OpenAPIService openAPIService;
+
+	// 创建订单，返回订单信息，编号等
+	@RequestMapping(value = "/pay.do")
+	@ResponseBody
+	public DataResponse pay(HttpServletRequest request) {
+
+		try {
+			DataResponse dr = new DataResponse();
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			Map<String, Object> remoteMap = new HashMap<String, Object>();
+			Enumeration<String> e = request.getParameterNames();
+			while (e.hasMoreElements()) {
+				String key = e.nextElement();
+				String value = request.getParameter(key);
+				remoteMap.put(key, value);
+			}
+			remoteMap.put("orderid", PayUtils.getOrderIdByUUId());			
+			resultMap.put("data", PayUtils.payOrder(remoteMap));
+			if (this.openAPIService.createOrder(remoteMap)) {
+				dr.setResponse(resultMap);
+				dr.setState(true);
+				return dr;
+			} else {
+				return new DataResponse(false, "创建订单失败！");
+			}
+		} catch (Exception e) {
+
+			return new DataResponse(false, "出错了！：" + e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/notifyPay.do", method = RequestMethod.POST)
+	public void notifyPay(HttpServletRequest request, HttpServletResponse response, PaySaPi paySaPi) {
+		// 保证密钥一致性
+		try {
+			if (PayUtils.checkPayKey(paySaPi)) {
+				System.out.println("/notifyPay.do更新数据库start！");
+				// 首先根据订单号查出充值信息，先给用户充值。
+				this.openAPIService.completeOrder(paySaPi);
+				System.out.println("/notifyPay.do更新数据库end！");
+			} else {
+				System.out.println("密钥错误！");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@RequestMapping("/getOrderStatus.do")
+	@ResponseBody
+	public DataResponse getOrderStatus(HttpServletRequest request, HttpServletResponse response, String orderid) {
+		DataResponse dr = new DataResponse();
+		try {
+			Map<String, String> map = this.openAPIService.getOrderStatus(orderid);
+			dr.setResponse(map);
+			dr.setState(true);
+		} catch (Exception e) {
+			return new DataResponse(false, "获取信息异常--" + e.getMessage());
+		}
+		return dr;
+	}
+}
